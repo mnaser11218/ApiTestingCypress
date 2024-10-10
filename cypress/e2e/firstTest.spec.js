43a54049592c3f3b16ad92aa2d9fcd7ad3dc748c
Mohammed N <mnaser11218@gmail.com>
2024-10-10 18:22:48 -0400

    completed verify corrrect requst... to successfully post an article

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -8,6 +8,10 @@ describe('test backend', () => {
     // first post an article then intercept the post requst
     cy.contains('a', 'New Article').click()
     cy.get('[formcontrolname="title"]').type('this is a title')
+    cy.get('[formcontrolname="description"]').type('this is a description')
+    cy.get('[formcontrolname="body"]').type('this is a body')
+    cy.get('[placeholder="Enter tags"]').type('this is a tag')
+    cy.get('button').contains('Publish Article').click()
   })
 
 })
