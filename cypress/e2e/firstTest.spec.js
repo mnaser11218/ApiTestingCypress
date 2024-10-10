55193c2c07fd108cc3e8ee9271d176de7e758cd1
Mohammed N <mnaser11218@gmail.com>
2024-10-10 18:25:47 -0400

    added intercept method in post article request

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -6,6 +6,10 @@ describe('test backend', () => {
 
   it.only('verify correct request and response',()=>{
     // first post an article then intercept the post requst
+
+    cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticle')
+
+    
     cy.contains('a', 'New Article').click()
     cy.get('[formcontrolname="title"]').type('this is a title')
     cy.get('[formcontrolname="description"]').type('this is a description')
