27b6e52f0e338ed1116b49348ccc3970f3b4dc8d
Mohammed N <mnaser11218@gmail.com>
2024-10-10 14:03:18 -0400

    added two custom mehtods: login to application and openHomepage

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it.only('Login to application ', ()=>{
+    cy.loginToApplication()
+  })
 })
