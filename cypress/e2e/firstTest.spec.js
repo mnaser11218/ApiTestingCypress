11f8ec2a89236cbc6d6796d7dcef27644ee8289a
Mohammed N <mnaser11218@gmail.com>
2024-10-10 19:22:14 -0400

    logging intercept method in verify correct request

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -9,13 +9,16 @@ describe('test backend', () => {
 
     cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticle')
 
-    
+
     cy.contains('a', 'New Article').click()
     cy.get('[formcontrolname="title"]').type('this is a title')
     cy.get('[formcontrolname="description"]').type('this is a description')
     cy.get('[formcontrolname="body"]').type('this is a body')
     cy.get('[placeholder="Enter tags"]').type('this is a tag')
     cy.get('button').contains('Publish Article').click()
+    cy.get('@postArticle').then(post=>{
+      console.log(post)
+    })
   })
 
 })
