b005be4130a2592a175f5eae3e1b47284c8085bb
Mohammed N <mnaser11218@gmail.com>
2024-10-10 12:35:14 -0400

    configured specPattern

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://conduit.bondaracademy.com/'
+    baseUrl: 'https://conduit.bondaracademy.com/',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
   },
 });
