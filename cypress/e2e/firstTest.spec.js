44889dffb31f089a46edc96c5a7d13aeedeec766
Mohammed N <mnaser11218@gmail.com>
2024-10-12 09:06:56 -0400

    added test to test tag-list contains 'Cypress' tag

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -33,6 +33,7 @@ describe('test backend', () => {
 
   it.only('verify popular tags are displayed', ()=>{
     cy.log('we logged in')
+    cy.get('[class="tag-list"]').should('contain', 'Cypress')
   })
 
 })
