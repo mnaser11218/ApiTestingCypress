404e778ba1a3dee72caed5ed14c1b9fb7fa8e00b
Mohammed N <mnaser11218@gmail.com>
2025-01-19 11:06:26 -0500

    updated comments in logintoapp method

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,19 +25,21 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('loginToApplication', ()=>{
     // modify to make headless authentication request
-   
+   // user credentials
     const userCredentials = {"user": 
         {
             "email": "mnaser11218@gmail.com",
             "password": "asd123"
             }
         }
+        // making a post request to login with user credentials
         cy.request('POST', 'https://conduit-api.bondaracademy.com/api/users/login',userCredentials)
   .its('body').then(body=>{
+    // store token and save it as an alias named token to call it in other files
     const token = body.user.token;
     cy.wrap(token).as('token')
 
-
+    // once you have logged in, now store the token in the localstorage by using the onBeforeLoad method to stay logged in.
     cy.visit('/', {
         onBeforeLoad (win) {
             win.localStorage.setItem('jwtToken', token)
